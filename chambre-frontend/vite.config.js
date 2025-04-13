bb165eb53eed7052a2ebddbe8764266e51a8f88d
soufienaessalah <soufien.ahmedessalah@esprit.tn>
2025-04-13 13:51:41 +0100

    Add proxy configuration in Vite config for backend API calls

--- chambre-frontend/vite.config.js
+++ chambre-frontend/vite.config.js
@@ -1,7 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/chambre': {
+        target: 'http://localhost:8089/foyer',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/chambre/, '/chambre')
+      }
+    }
+  }
 })
