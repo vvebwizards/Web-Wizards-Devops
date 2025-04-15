592adbb7dfd38bc91a41b74dec1df2ea27589959
Riahi Yassine <yassine.riahi@esprit.tn>
2025-04-15 17:24:19 +0100

    change

--- backend/src/main/java/tn/esprit/foyer/FoyerApplication.java
+++ backend/src/main/java/tn/esprit/foyer/FoyerApplication.java
@@ -2,8 +2,11 @@ package tn.esprit.foyer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -13,6 +16,17 @@ public class FoyerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoyerApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("*");
+			}
+		};
 	}
 
 }
